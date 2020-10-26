2153f174f3e71824c84f2c07ad37be03889fe52d
a19131100 <gamzemadan@std.iyte.edu.tr>
2020-10-26 09:02:56 +0000

    exercises 1,2 and 3

--- lab3/example2.py
+++ lab3/example2.py
@@ -2,10 +2,10 @@ num1= float(input("Please enter the first number:"))
 num2= float(input("Please enter the second number:"))
 num3= float(input("Please enter the third number:"))
 
-if num1<num2 and num1<num3:
+if num1<=num2 and num1<=num3:
   min_value= num1
 
-elif num2<num1 and num2<num3:
+elif num2<=num1 and num2<=num3:
   min_value= num2
 
 else:
