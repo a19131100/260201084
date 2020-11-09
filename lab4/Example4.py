1fa9cbe432e6a9ab70d00377506d74846cb5218f
a19131100 <gamzemadan@std.iyte.edu.tr>
2020-11-09 09:23:07 +0000

    exercise 5 is done

--- lab4/Example4.py
+++ lab4/Example4.py
@@ -2,7 +2,6 @@ a= int(input("Please enter the first number: "))
 b= int(input("Please enter the second number: "))
 mult=1
 
-
 for i in range(b):
   mult *= a
 
---
+++ lab4/Example5.py
@@ -0,0 +1,9 @@
+n= int(input("Please enter the number 'n': "))
+
+factor= 1
+
+for i in range(n):
+  i+=1
+  factor *= i
+
+print(factor)
