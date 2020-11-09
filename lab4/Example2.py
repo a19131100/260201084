939665344f2959f7be027d637a575812e1cbeaef
a19131100 <gamzemadan@std.iyte.edu.tr>
2020-11-09 09:06:41 +0000

    exercise 2 is done

--- lab4/Example2.py
+++ lab4/Example2.py
@@ -1,7 +1,7 @@
 year= int(input("Please enter the year: "))
 
 if year%4==0:
-  if year%400==0:
+  if year%400!=0:
     print("It's a century year, not a leap year")
   else:
     print("It's a leap year")
