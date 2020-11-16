033a01a6e2edbc86e15c54cf902a77a494cd92a3
a19131100 <gamzemadan@std.iyte.edu.tr>
2020-11-16 08:58:05 +0000

    example 1

---
+++ lab5/Example1.py
@@ -0,0 +1,12 @@
+integer_= int(input("Please enter an integer: "))
+while integer_<= 0:
+  integer_= int(input("Please enter a valid integer: "))
+
+while integer_>10:
+  integer_= int(input("Please enter an integer less than 10: "))
+
+
+for i in range(10):
+  i= int(i) + 1
+  print( integer_ , "x", i, "=", i*integer_)
+
