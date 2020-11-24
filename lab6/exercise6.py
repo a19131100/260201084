95b85ca3e2ecac263bf3a8dc687b84c1992b1aa0
a19131100 <gamzemadan@std.iyte.edu.tr>
2020-11-24 12:54:01 +0000

    All exercises are done

---
+++ lab6/exercise6.py
@@ -0,0 +1,16 @@
+length= int(input("Write a number for the matrix: "))
+matrix=[]
+for i in range(length):
+  matrix.append([0] * length)
+
+for row in range(length):
+  for coloumn in range(length):
+    num= int(input("Please enter a number: "))
+    matrix[row][coloumn]= num
+
+sum_of_main=0
+for i in range(length):
+  sum_of_main += matrix[i][i]
+
+print(matrix)
+print("The sum of the main diagonal is: ", sum_of_main)
