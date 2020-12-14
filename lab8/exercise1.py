381f958ea38cb5b4f87b3e9042d0c15381a45526
a19131100 <gamzemadan@std.iyte.edu.tr>
2020-12-14 08:44:25 +0000

    1st exercise

---
+++ lab8/exercise1.py
@@ -0,0 +1,9 @@
+def sum_function(a_list):
+  sum_of_elements = 0
+  for i in a_list:
+    sum_of_elements += i
+
+  return sum_of_elements**2
+
+
+print(sum_function([12, -7, 5, -89.4, 3, 27, 56, 57.3]))
