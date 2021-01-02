2c8abe66f5f51bcf1f7cbcbfecde802b633cb2f2
a19131100 <gamzemadan@std.iyte.edu.tr>
2021-01-02 17:33:30 +0000

    exercises

---
+++ lab10/exercise4-5.py
@@ -0,0 +1,17 @@
+def pascal_line(n):
+  if n == 0:
+    return []
+  elif n == 1:
+    return [1]
+  else:
+    new_row = [1]
+    result = pascal_line(n-1)
+    last_row = result[:-1]
+    for i in range(len(last_row)-1):
+      new_row.append(last_row[i] + last_row[i+1])
+      new_row += [1]
+    result.append(new_row)
+  return result
+
+
+print(pascal_line(5))
